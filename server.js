c84021e7a3a70ec58dd325e44bb851b64639b237
Jack Ceballos <jackrceballos@gmail.com>
2021-02-27 12:51:16 -0500

    Working on server file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,7 +10,9 @@ app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-const exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT: ${PORT}`);
+});
