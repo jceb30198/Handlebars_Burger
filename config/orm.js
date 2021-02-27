6e35e58bbb9b9b54c4eabc995e30001bc911044c
Jack Ceballos <jackrceballos@gmail.com>
2021-02-27 16:10:00 -0500

    Finished selectAll orm

--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,16 @@
 const connection = require("./connection");
 
 const orm = {
-    selectAll: () => {
-        let query = 
+    selectAll: (tableName, cb) => {
+        let query = "SELECT * FROM ?";
+
+        connection.query(query, [tableName], (err, result) => {
+            if(err) throw err;
+            cb(result);
+        })
     },
     insertOne: () => {
-
+        let query = ""
     },
     updateOne: () => {
 
