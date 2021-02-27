ee8bf0f25059c65f7e459652d8cfc20335acd1d6
Jack Ceballos <jackrceballos@gmail.com>
2021-02-27 14:24:04 -0500

    Creating a connection to the mysql database

---
+++ config/connection.js
@@ -0,0 +1,10 @@
+const { connect } = require("http2");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burger_db"
+});
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Ham Burger", true), ("Cheese Burger", false), ("Beyond Burger", false);
