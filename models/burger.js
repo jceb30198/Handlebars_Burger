9096e351ce8eea41ca4a3d5500f5bf6355368557
Jack Ceballos <jackrceballos@gmail.com>
2021-02-27 17:30:17 -0500

    Finished the model for the burger

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,19 @@
 const orm = require("../config/orm");
 
 const burger = {
-    
-}
+    all: function(cb) {
+        orm.selectAll("burgers", function(results) {
+            cb(results);
+        })
+    },
+    insert: function(nameCol, value, cb) {
+        orm.insertOne("burgers", nameCol, value, function(results) {
+            cb(resuls);
+        })
+    },
+    update: function(burgerId, cb) {
+        orm.updateOne("burgers", burgerId, function(results) {
+            cb(results);
+        })
+    }
+}   
