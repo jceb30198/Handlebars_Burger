669fb339cc25f7f693fc762d492d30bbce9b4239
Jack Ceballos <jackrceballos@gmail.com>
2021-03-01 14:07:14 -0500

    Working on the front end javascript

---
+++ public/assets/JS/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    const eatBtn = $(".eat-button");
+    const createBtn = $("#create-button");
+
+    eatBtn.click(() => {
+        console.log("Clicked Eat Button!");
+    });
+
+    createBtn.click(() => {
+
+    });
+})
