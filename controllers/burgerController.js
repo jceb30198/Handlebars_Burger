bdfbddada856b82eb2b23a88feaa7ee51e88ee62
Jack Ceballos <jackrceballos@gmail.com>
2021-02-28 19:50:15 -0500

    Added router to test on POSTMAN

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -3,3 +3,13 @@ const burger = require("../models/burger");
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    burger.all((data) => {
+        const hbsObject = {
+            burgers: data
+        }
+        console.log(hbsObject)
+    });
+});
+
+module.exports = router;
